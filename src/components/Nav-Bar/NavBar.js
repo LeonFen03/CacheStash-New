9493dee74b5f57b80758f2ad386eb8e8eba0540b
LeonFen03 <LeonFen03@gmail.com>
2024-01-17 18:16:22 -0500

    Possible Navigations

--- src/components/Nav-Bar/NavBar.js
+++ src/components/Nav-Bar/NavBar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 function NavBar () {
     return (<div><nav>
         <ul>
             <li>
-
+                About
             </li>
+            <li>
+                Price plans
+            </li>
+            <li>
+               Public Gallery
+            </li>
+            <li>Login <AccountCircle /></li>
         </ul>
     </nav>
     <Outlet />
