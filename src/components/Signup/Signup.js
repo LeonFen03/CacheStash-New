5684155cbfd00d8838f80ea30a0bc5249fbf1ee1
LeonFen03 <LeonFen03@gmail.com>
2024-01-17 18:04:01 -0500

    Possible layout and design for Landing page signup

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { TextField } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import InputAdornment from '@mui/material/InputAdornment';
+import {Button} from '@mui/material';
+import './Signup.css'
 function Signup () {
-    return (<div>
-        
+    return (<div className="signup-container"> 
+    <h2>Sign up</h2>
+    <div className="input-box">
+        <TextField
+        id="input-with-icon-textfield"
+        label="Email"
+        style={{maxWidth:'400px',width:'90%',minWidth:'300px',marginBottom:'30px'}}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
+        </div>
+        <div className="input-box">
+        <TextField
+        id="input-with-icon-textfield"
+        label="Username"
+        style={{maxWidth:'400px',width:'90%',minWidth:'300px',marginBottom:'30px'}}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
+      </div>
+      <div className="input-box">
+      <TextField
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="standard"
+          style={{maxWidth:'400px',width:'90%',minWidth:'300px',marginBottom:'30px'}}
+        />
+        </div>
+        <div className="input-box">
+            <Button variant="contained">Submit</Button>
+        </div>
     </div>)
 }
 
