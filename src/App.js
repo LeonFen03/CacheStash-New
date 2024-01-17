f2d68a0698a9ccadcd743117a2527c0131a27556
LeonFen03 <LeonFen03@gmail.com>
2024-01-17 14:31:36 -0500

    Added React Router, Modified App.css, added design for landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { AnimatePresence } from 'framer-motion';
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
 function App() {
+  const appRouter = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" > 
+      <Route path="/Home" element={<LandingPage />} />
+    </Route>))
+
   return (
-    <div className="App">
-    
-    </div>
-  );
+    <AnimatePresence >
+  <RouterProvider router={appRouter} />
+  </AnimatePresence>);
 }
 
 export default App;
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './LandingPage.css';
 function LandingPage () {
-    return(<main>
-
+    return(<main className="landing-bg">
+        <div className="brand-images">
+s
+        </div>
+        <div className="sign-up-container">
+s
+        </div>
     </main>)
 }
 
