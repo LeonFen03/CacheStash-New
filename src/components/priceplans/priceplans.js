2541e5dc248f817a258465280a82a00ad8ba1ed5
campbell1105 <campbell1105@gmail.com>
2024-01-21 11:51:02 -0500

    pricing2

--- src/components/priceplans/priceplans.js
+++ src/components/priceplans/priceplans.js
@@ -5,18 +5,23 @@ function PricePlans() {
     return (<div className="card-container">
         <div className="card">
             <p>10 GB   - $5 / mo.</p>
+            <Button  variant='contained'>Get started</Button>
         </div>
         <div className="card">
         <p>200 GB  - $25 / mo.</p>
+        <Button variant='contained'>Get started</Button>
         </div>
         <div className="card">
         <p>600 GB  - $45 / mo.</p>
+        <Button variant='contained'>Get started</Button>
         </div>
         <div className="card">
         <p>1 TB  -  $60  / mo.</p>
+        <Button variant='contained'>Get started</Button>
         </div>
         <div className="card">
-        <p>12 TB  -  $120  / mo.</p>
+        <p>10 GB   - $5 / mo.</p>
+        <Button variant='contained'>Get started</Button>
         </div>
     </div>)
 }
