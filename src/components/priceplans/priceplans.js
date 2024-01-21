74cd0b3823b6e107f75e56637c136bf087293e93
campbell1105 <campbell1105@gmail.com>
2024-01-21 11:41:24 -0500

    pricing cards

--- src/components/priceplans/priceplans.js
+++ src/components/priceplans/priceplans.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './priceplans.css';
+import { Button } from '@mui/material';
 function PricePlans() {
-    return (<div>
-        
+    return (<div className="card-container">
+        <div className="card">
+            <p>10 GB   - $5 / mo.</p>
+        </div>
+        <div className="card">
+        <p>200 GB  - $25 / mo.</p>
+        </div>
+        <div className="card">
+        <p>600 GB  - $45 / mo.</p>
+        </div>
+        <div className="card">
+        <p>1 TB  -  $60  / mo.</p>
+        </div>
+        <div className="card">
+        <p>12 TB  -  $120  / mo.</p>
+        </div>
     </div>)
 }
 
