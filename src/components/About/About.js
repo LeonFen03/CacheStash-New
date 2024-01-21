e442c549f0a118271494605b32a63c9be8471eb3
Hannah Mills <millsh56@gmail.com>
2024-01-20 23:28:49 -0500

    adding links for buttons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { colors } from './colors'; 
+import { Link } from 'react-router-dom'
 
 function AboutPage() {
   return (
@@ -34,9 +35,8 @@ function AboutPage() {
           paragraph
           sx={{ fontFamily: 'Gibson Light, sans-serif' }}
         >
-          Welcome to our app! Here, you'll find something extraordinary about the
-          collection below—its contents, the creators, and the story behind it.
-          We're here to make your experience memorable and enjoyable.
+          Welcome to CacheStash a little project thats helping you out in a big way! CacheStash is your new location for storing files. You can easily store, sort, and share your images to access anytime on any device!
+          To get started simply sign up for a free profile and start storing your images today!
         </Typography>
         <Stack
           sx={{ pt: 4 }}
@@ -44,12 +44,17 @@ function AboutPage() {
           spacing={2}
           justifyContent="center"
         >
+            <Link to="/LandingPage">
           <Button variant="contained" color="primary">
-            Discover More
+            Home
           </Button>
+          </Link>
+          <Link to="SignUp">
           <Button variant="outlined" color="primary">
             Get Started
           </Button>
+          </Link>
+          {/* might need to fix routes for these */}
         </Stack>
         <Box sx={{ mt: 4, textAlign: 'center' }}>
           {/* should we use the logo here or another image? */}
