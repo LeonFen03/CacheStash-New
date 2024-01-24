198984e5de30b812b2eec69a7bd3359d7235dd97
Hannah Mills <millsh56@gmail.com>
2024-01-23 20:29:09 -0500

    implementing responsive styling for mobile tabs

--- src/components/Utility-Functions/MaterialUISelectCategory.js
+++ src/components/Utility-Functions/MaterialUISelectCategory.js
@@ -5,8 +5,8 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function BasicSelect() {
-
+export default function BasicSelect({categories}) {
+categories = [...categories]
   const handleChange = (event) => {
   };
 
